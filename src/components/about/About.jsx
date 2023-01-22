919c80f1401739e4875ae105cee092afd035a05c
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2023-01-22 15:56:18 +0200

    change info about my

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './about.css';
 import AboutImg from '../../assets/about.jpg';
-import CV from '../../assets/John-Cv.pdf'
+import CV from '../../assets/CV-Dima-Oleinik.pdf'
 import Info from "./info";
 
 const About = () => {
