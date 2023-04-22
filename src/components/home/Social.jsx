c790383f49ca046669a856167ecb1589f38cf322
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2023-04-22 20:26:49 +0300

    add animation

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Social = () => {
     return (
         <div className='home__social opacity'>
-            <a href="https://www.instagram.com/__doleinik/" target='_blank' className='home__social-icon'>
+            <a href="https://www.instagram.com/__doleinik/" target='_blank'  rel="noreferrer" className='home__social-icon'>
                 <i className='uil uil-instagram'></i>
             </a>
-            <a href="https://t.me/doleinik" target='_blank' className='home__social-icon'>
+            <a href="https://t.me/doleinik" target='_blank'  rel="noreferrer" className='home__social-icon'>
                 <i className='uil uil-telegram'></i>
             </a>
-            <a href="https://github.com/doleinik" target='_blank' className='home__social-icon'>
+            <a href="https://github.com/doleinik" target='_blank'  rel="noreferrer" className='home__social-icon'>
                 <i className='uil uil-github-alt'></i>
             </a>
         </div>
