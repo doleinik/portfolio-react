c4ff4973f703e4ee0e3480fbf27673c27c1d6e7b
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2023-04-22 20:33:28 +0300

    add animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <header className='header '>
             <nav className="nav container swipe-top">
-                <a href="index.html" className="nav__logo">
+                <a href="/" className="nav__logo">
                     Oleinik
                 </a>
 
