723e1d0ba41557e67673012e5f6cdb0183cb44be
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2023-04-23 16:39:14 +0300

    preloader

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
     return (
         <header className='header '>
-            <nav className="nav container swipe-top">
+            <nav className="nav container">
                 <a href="/" className="nav__logo">
                     Oleinik
                 </a>
