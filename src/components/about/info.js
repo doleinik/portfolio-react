d18dba57a4df8f4197674a9fa61a660b42e287a9
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2023-04-23 22:06:03 +0300

    preloader

--- src/components/about/info.js
+++ src/components/about/info.js
@@ -28,14 +28,14 @@ const Info = () => {
             </div>
 
             <div className="about__box">
-                <i className='bx bx-support about__icon'></i>
+                <i className='bx bx-star about__icon'></i>
 
                 <h3 className="about__title">
-                    Support
+                    Quality
                 </h3>
 
                 <span className="about__subtitle">
-                    Online 24/7
+                    Guarantee
                 </span>
             </div>
         </div>
