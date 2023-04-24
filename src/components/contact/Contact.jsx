96f154c8022ec606d28bb357adc3ebad39e287cf
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2023-04-24 18:14:05 +0300

    change info about me

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,9 +19,9 @@ const Contact = () => {
 
             <div className="contact__container container grid">
                 <div className="contact__content">
-                    <h3 className="contact__title">
-                        Talk to me
-                    </h3>
+                    {/*<h3 className="contact__title">*/}
+                    {/*    Talk to me*/}
+                    {/*</h3>*/}
 
                     <div className="contact__info">
                         <div className="contact__card">
@@ -63,9 +63,9 @@ const Contact = () => {
                 </div>
 
                 <div className="contact__content">
-                    <h3 className="contact__title">
-                        Write me your project
-                    </h3>
+                    {/*<h3 className="contact__title">*/}
+                    {/*    Write me your project*/}
+                    {/*</h3>*/}
 
                     <form ref={form} onSubmit={sendEmail} className="contact__form">
                         <div className="contact__form-div">
