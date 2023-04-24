72aa15dd505522ada80044c9e376d13fa5578fa5
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2023-04-24 23:48:10 +0300

    change info about me

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -28,9 +28,9 @@ const Contact = () => {
                             <i className="bx bx-envelope contact__cars-icon"></i>
 
                             <h3 className="contact__card-title">Email</h3>
-                            <span className="contact__card-data">doleinika@gmail.com</span>
+                            <span className="contact__card-data">devdoleinik@gmail.com</span>
 
-                            <a href="mailto:doleinika@gmail.com" className="contact__button">
+                            <a href="mailto:devdoleinik@gmail.com" className="contact__button">
                                 Write me
                                 <i className="bx bx-right-arrow-alt contact__button-icon"></i>
                             </a>
