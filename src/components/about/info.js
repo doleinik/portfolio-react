e110ddfc0e6f187fe0c5629fe0b1b6c3c8e29338
Doleinik <57624563+doleinik@users.noreply.github.com>
2024-11-27 17:38:07 +0100

    add item

--- src/components/about/info.js
+++ src/components/about/info.js
@@ -1,45 +1,33 @@
 import React from "react";
 
 const Info = () => {
-    return (
-        <div className='about__info grid'>
-            <div className="about__box">
-                <i className='bx bx-award about__icon'></i>
+ return (
+  <div className="about__info grid">
+   <div className="about__box">
+    <i className="bx bx-award about__icon"></i>
 
-                <h3 className="about__title">
-                    Experience
-                </h3>
+    <h3 className="about__title">Experience</h3>
 
-                <span className="about__subtitle">
-                    2 Years Working
-                </span>
-            </div>
+    <span className="about__subtitle">4 Years Working</span>
+   </div>
 
-            <div className="about__box">
-                <i className='bx bx-briefcase-alt about__icon'></i>
+   <div className="about__box">
+    <i className="bx bx-briefcase-alt about__icon"></i>
 
-                <h3 className="about__title">
-                    Completed
-                </h3>
+    <h3 className="about__title">Completed</h3>
 
-                <span className="about__subtitle">
-                    20+ Projects
-                </span>
-            </div>
+    <span className="about__subtitle">20+ Projects</span>
+   </div>
 
-            <div className="about__box">
-                <i className='bx bx-star about__icon'></i>
+   <div className="about__box">
+    <i className="bx bx-star about__icon"></i>
 
-                <h3 className="about__title">
-                    Quality
-                </h3>
+    <h3 className="about__title">Quality</h3>
 
-                <span className="about__subtitle">
-                    Guarantee
-                </span>
-            </div>
-        </div>
-    )
-}
+    <span className="about__subtitle">Guarantee</span>
+   </div>
+  </div>
+ );
+};
 
 export default Info;
